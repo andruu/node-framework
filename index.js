63b25256fc998e2ec5598d904f0f77075672ee69
Andrew Weir <andru.weir@gmail.com>
2012-08-18 12:38:01 -0400

    Changed structure of folders

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-var server   = require('./server'),
-    Router   = require('./router')
+var server   = require('./lib/server'),
+    Router   = require('./lib/router'),
     router   = new Router(),
-    Renderer = require('./renderer'),
+    Renderer = require('./lib/renderer'),
     renderer = new Renderer();
 
 renderer.setEngine('jade');
-
-router.get('/address/:city/:state/:zip', function (request, response) {
-  response.write(JSON.stringify(request.params));
-  response.end('called the address view');
-});
-
-router.get('/users/:slug/:id', function (request, response) {
-  response.write(JSON.stringify(request.params));
-  response.end('called the users view with id');
-});
-
-router.get('/users/:slug', function (request, response) {
-  response.write(JSON.stringify(request.params));
-  response.end('called the users view');
-});
-
-router.get('/posts/:slug', function (request, response) {
-  response.write(JSON.stringify(request.params));
-  response.end('called the posts view');
-});
 
 router.get('/', function (request, response) {
   response.render('index');
