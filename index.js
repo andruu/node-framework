62165cd2cdacaf9749e543927477b05b6fb9730d
andruu <andru.weir@gmail.com>
2013-01-23 19:35:23 -0500

    Lots of refactoring

--- index.js
+++ index.js
@@ -3,24 +3,47 @@ var server   = require('./lib/server'),
     router   = new Router(),
     Renderer = require('./lib/renderer'),
     renderer = new Renderer(),
-    util     = require('util');
+    util     = require('util'),
+    port     = process.env.PORT || 8888;
 
 renderer.setEngine('jade');
 
 router.get('/', function (request, response) {
-  console.log(util.inspect(request.getCookie('name')));
   response.render('index', {method: request.method});
 });
 
-router.get('/posts', function (request, response) {
-  response.render('index', {method: request.method});
+router.get('/:page', function (request, response) {
+  response.render('pages/' + request.params.page, {
+    title: request.params.page,
+    page: request.params.page
+  });
 });
+
+router.get('/posts', function (request, response) {
+  response.format({
+    html: function () {
+      response.render('index', {method: request.method});
+    },
+    json: function () {
+      response.json({
+        first_name: 'andrew',
+        last_name: 'weir'
+      });
+    },
+    'text/javascript': function () {
+      response.send('console.log("Hello World");');
+    }
+  });
+});
+
 router.post('/posts', function (request, response) {
   response.render('index', {method: request.method});
 });
+
 router.put('/posts', function (request, response) {
   response.render('index', {method: request.method});
 });
+
 router.delete('/posts', function (request, response) {
   response.render('index', {method: request.method});
 });
@@ -34,18 +57,19 @@ router.get('/users/:first_name/:last_name', function (request, response) {
     first_name: request.params.first_name,
     last_name: request.params.last_name
   }));
+
   response.render('users/show', {
     title: 'Hello World',
-    first_name: request.params.first_name,
-    last_name: request.params.last_name
+    first_name: request.params.first_name.titleize(),
+    last_name: request.params.last_name.titleize()
   });
 });
 
 router.get('/users/new/:id', function (request, response) {
-  console.log(request.params.id);
   response.render('users/new', {
-    id: request.params.id
+    title: 'Hello World'
   });
 });
 
-server.start(router, 8888, renderer);
+
+server.start(router, port, renderer);
